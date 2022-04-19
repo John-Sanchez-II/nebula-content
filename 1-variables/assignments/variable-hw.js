78de2af5fb9520a557956f2a618cb959c1198f4d
oneIwant <jsanchez0937@gmail.com>
2022-04-19 00:44:43 -0400

    variable-hw.js

---
+++ 1-variables/assignments/variable-hw.js
@@ -0,0 +1,42 @@
+// Create a .js file that satisfies assignment below 
+// Copy the final assignment text into this assignment to submit
+// For each exercise you'll want to log the result to the console to check your work
+
+//1. Create a set of variables that define an animal (age, height, name, etc...). Must be at least 6 variables.
+
+const lizard = "Komodo Dragon (Carnivores)"
+
+const group = "Reptillia"
+
+const age = "Lifespan - Up to 30 years"
+
+const height = "Height - (Male) 10ft  (Female) 6ft"
+
+const weight = "Weight - (Male) 174-201 lbs. (Female) 150-161 lbs."
+
+const speed = "Speed-12 mph (Sprint)"
+
+const reptile =  `${lizard}, ${group} 
+${age}, ${height}
+${weight}, ${speed}`
+console.log (reptile)
+//2. Create a set of variables that define a vehicle. Must be at least 6 variables. 
+
+const model = "1930 Mercedes-Benz 710 SSK Trossi Roadster"
+
+const designer = "Ferdinand Porsche"
+
+const quantity = "5 left in the world"
+
+const built = "1928-1930"
+
+const price = "$800,000-$1,100,000"
+
+const transmission = "4 Speed Manuel"
+
+console.log(model)
+console.log(designer)
+console.log(quantity)
+console.log(built)
+console.log(price)
+console.log(transmission)
