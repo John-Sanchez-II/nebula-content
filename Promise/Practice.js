6e3bc9e813c7305c8a0fdacfd4f873b48f1992f1
oneIwant <jsanchez0937@gmail.com>
2022-06-29 23:01:35 -0400

    promises

---
+++ Promise/Practice.js
@@ -0,0 +1,100 @@
+console.log(cleanedDishes)
+setTimeout(() => { console.log(cleanedDishes)}, 5001)
+
+
+// you and a friend want to have dinner
+// you're going to make soup but you also want tacos
+// it makes more sense to make the soup While your friend gets tacos, not after
+// your friend is the promise
+// fulfilled: got tacos, omw
+
+
+
+// 1.   What is the order of output?
+setTimeout(() => {
+    console.log('a');
+}, 1);
+console.log('b');
+
+new Promise( (res,rej) => {res('c')}).then((val) => console.log('d', val));
+console.log('e');
+
+setTimeout(() => {
+  console.log('f');
+}, 0);
+
+setTimeout(() => {
+    console.log('g');
+}, 3);
+
+new Promise( (res) => {res('h')}).then(res => console.log(res, 'i'))
+// b, e ,d c, h i, promisesfulfilled, a, f, g,
+
+// 2.   Create a promise for something you may, or may not do.
+let duetPromise = false
+let myPromise = new Promise( (resolve, reject) => {
+        setTimeout(()=> {
+            if (singPromise){
+                resolve('I can match your pitch')
+            } else {
+                reject('I"ll never be able to sing with you(cries violently in fetal position)')
+            }
+        }, 5 * 2000)
+        
+    })
+    
+console.log (duetPromise)
+// 3.   Create a new promise for something you may or may not do. 
+//      After a few seconds log whether it has been done.
+
+
+// 4.   Using the prior example use .then and .catch to handle responses and rejections
+
+let duetpromise = new Promise(function (resolve)  {
+        setTimeout(()=> resolve 
+                ("I can finally match your pitch"),1000)
+             })
+
+    duetpromise.then(
+        result=> alert(result),
+        error => alert (error)
+    )
+// 5.   What does a promise resolve do?
+// returns a Promise object that is resolved with a given value
+
+// 6.   Using HTML & CSS, write a promise that, after 5 seconds changes the existing text on the web-page
+const trick =[
+    {title: "Trick 1", body: "Tada!!!"}
+]
+function magic(){
+    setTimeout( () => {
+        let output = " ";
+        trick.forEach( (post) => {
+            output+= `<li>${trick.title}: ${post.body}</li>`
+        })
+        const bin = document.querySelector(`.bin`)
+        bin.innerHTML= output
+    }, 5000 )
+}
+
+// 7. What is the output of the following code?
+const promise = new Promise(res => res(2));
+promise.then(v => {
+    console.log(v);
+    return v * 2;
+})
+.then(v => {
+    console.log(v);
+    return v * 2;
+})
+.then(v => {
+    console.log(v);
+    return v * 2;
+})
+.finally(v => {
+    console.log(v)
+})
+
+// 2
+// 4
+// not sure what it's asking
