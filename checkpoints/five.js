d9ab65a9af41cf267c7a6c5df9d5ba9fac29593d
oneIwant <jsanchez0937@gmail.com>
2022-07-01 23:01:43 -0400

    checkpoint

---
+++ checkpoints/five.js
@@ -0,0 +1,35 @@
+// 1.Which cohort are you in?
+// Melissa Jeremie
+
+// 2.What is a function and why is it useful?
+
+// reusable chunks of code that offer a way to structure your code, once a function is created, it can be looked at as a mini program that is ready to be executed at any time and helps to break down a complicated program into smaller manageable parts to make a program less complicated 
+
+// 3.Explain a parameter and an argument 
+// a parameter is a placeholder for an input
+// arguments are the real values passed to the function
+
+// 4.Write a function declaration, a function expression, and an arrow function
+
+
+// 5.What is a higher order function and why do we use them?
+
+
+// 6. Declare an object that a school system may use to represent a student
+
+
+// 7.What is a property and what is a method in relation to objects?
+
+
+// 8.Describe a key-value pair
+
+
+// 9.Create an object with at least 5 key pairs and then access 3 of those keys
+
+
+// 10.Create a method using your object
+
+
+// 11.Create a function that allows you to add new properties to your object
+
+
